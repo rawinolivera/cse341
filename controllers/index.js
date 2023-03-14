28d34227b7947f75a8601317182b3917b83533df
rawinolivera <oli18001@byui.edu>
2023-03-14 16:57:42 -0500

    W02 complete

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-const awesomeFunction = (req, res, next) => {
-    res.json('Milexa Torres');
-};
-
-const returnAnotherPerson = (req, res, next) => {
-    res.json('Super awesome person');
-};
-
-module.exports = { awesomeFunction, returnAnotherPerson };
--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-const port = process.env.PORT || 3000
-
-app.use('/', require('./routes'));
-
-app.listen(port, () =>{
-    console.log(`server is running on port ${port}`);
-})
