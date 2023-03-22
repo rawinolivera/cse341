89a95f3048493dbc4933b1705b36b3dbc8783a7b
rawinolivera <oli18001@byui.edu>
2023-03-22 15:12:06 -0500

    L03 finished

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -22,4 +22,62 @@ const getSingle = async (req, res, next) => {
   });
 };
 
-module.exports = { getAll, getSingle };
+// post request
+const newContact = async (req, res, next) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb
+    .getDb()
+    .db('contacts')
+    .collection('contacts')
+    .insertOne(contact);
+  if(response.acknowledged){
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || "Error, contact not created");
+  }
+}
+
+//put request
+const updateContact = async (req, res, next) => {
+  const contactId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb
+    .getDb()
+    .db('contacts')
+    .collection('contacts')
+    .replaceOne({ _id: contactId }, contact);
+  if(response.acknowledged){
+    res.status(204).json(response);
+  } else {
+    res.status(500).json(response.error || "Error, contact not updated");
+  }
+}
+
+//delete request
+const deleteContact = async (req, res, next) => {
+  const contactId = new ObjectId(req.params.id);
+  const response = await mongodb
+    .getDb()
+    .db('contacts')
+    .collection('contacts')
+    .deleteOne({ _id: contactId }, true);
+  if(response.acknowledged){
+    res.status(200).json(response);
+  } else {
+    res.status(500).json(response.error || "Error, contact not deleted");
+  }
+}
+
+module.exports = { getAll, getSingle, newContact, updateContact, deleteContact };
